6ba6c46a55902b0d1b20f2f75a48f2d4ef1619f1
cosminEmil <132258074+cosminEmil@users.noreply.github.com>
2023-06-08 13:06:07 +0300

    Update hidden-button.js

--- hidden-button.js
+++ hidden-button.js
@@ -1,16 +1,20 @@
+function WorL(currentButton, randomIndex) {
+        if (randomIndex == currentButton) {
+            alert("Winner");
+        } else {
+            alert("Loser");
+        }
+    }
+
 function generateButtons() {
     let buttonsNr = document.getElementById("text_box").value;
     let randomIndex = Math.floor(Math.random() * buttonsNr);
     let btnContainer = document.getElementById("buttons");
 
-    for (let i = 0; i < buttonsNr; ++i) {
+    for (let currentButton = 0; currentButton < buttonsNr; ++currentButton) {
         let newButton = document.createElement("button");
         newButton.onclick = function() {
-            if (randomIndex == i) {
-                alert("Winner");
-            } else {
-                alert("Loser");
-            }
+            WorL(currentButton, randomIndex);
         }
         newButton.innerText = "Click me";
         btnContainer.appendChild(newButton);
